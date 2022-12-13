5fe9b492d94330011b3d1acb6e82010f5ee92f6f
Jethro Flanagan <jethroflanagan@gmail.com>
2022-12-13 09:56:21 +0200

    Remove remnants of NVChad files

--- lua/custom/plugins/gitsigns.lua
+++
@@ -1,7 +0,0 @@
-return {
-  requires = { "petertriho/nvim-scrollbar" },
-  config = function()
-    require("gitsigns").setup()
-    require("scrollbar.handlers.gitsigns").setup()
-  end,
-}
--- lua/custom/plugins/nvim-hlslens.lua
+++
@@ -1,9 +0,0 @@
-return {
-  requires = { "nvim-lua/plenary.nvim", "petertriho/nvim-scrollbar" },
-  config = function()
-    -- require('hlslens').setup() is not required
-    require("scrollbar.handlers.search").setup {
-      -- hlslens config overrides
-    }
-  end,
-}
