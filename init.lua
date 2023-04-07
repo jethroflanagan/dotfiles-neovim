1e78fbc422f6fff1de26963208771dca1aa5ca2c
Jethro Flanagan <jethroflanagan@gmail.com>
2023-04-07 14:21:03 +0200

    Mix of kickstart and lazy-nvim

--- init.lua
+++ init.lua
@@ -1,2 +1,25 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.lazy")
+-- bootstrap lazy.nvim, LazyVim and plugins
+
+-- Install package manager
+--    https://github.com/folke/lazy.nvim
+--    `:help lazy.nvim.txt` for more info
+local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system {
+    'git',
+    'clone',
+    '--filter=blob:none',
+    'https://github.com/folke/lazy.nvim.git',
+    '--branch=stable', -- latest stable release
+    lazypath,
+  }
+end
+vim.opt.rtp:prepend(lazypath)
+
+require('lazy').setup({
+  { import = 'plugins' },
+})
+
+require 'config.options'
+require 'config.keymaps'
+require 'config.autocmds'
--- lua/plugins-disabled/monark.lua
+++
@@ -1,1 +0,0 @@
-return { "doums/monark.nvim" }
